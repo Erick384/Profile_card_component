ec0e802d81c90a2bff75704c3d6c0cc8c7844525
Erick384 <erickwachuma@gmail.com>
2023-06-20 23:41:58 +0300

    Uploading commits

--- Profile_Card/src/App.jsx
+++ Profile_Card/src/App.jsx
@@ -6,6 +6,17 @@ function App() {
   return (
     <body>
       <div className="container">
+        <div className="top"></div>
+        <div className="profilePic"></div>
+        <div className="content">
+          <div>
+            <h2>Victor Crest</h2>
+            <span>26</span>
+          </div>
+          <div>
+            <span>London</span>
+          </div>
+        </div>
         <Bottom />
       </div>
       <Footer />
