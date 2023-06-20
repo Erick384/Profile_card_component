544f72856067a4a85b29ff7a009dd64caa8f64c2
Erick384 <erickwachuma@gmail.com>
2023-06-20 23:20:05 +0300

    Uploading commits

---
+++ Profile_Card/src/Component/Bottom/Bottom.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Bottom.css"
+
+const Bottom = () => {
+  return (
+    <div className="bottom">
+        <div>
+            <h3>80k</h3>
+            <span>Followers</span>
+        </div>
+        <div>
+            <h3>803k</h3>
+            <span>Likes</span>
+        </div>
+        <div>
+            <h3>1.4k</h3>
+            <span>Photos</span>
+        </div>
+    </div>
+  )
+}
+
+export default Bottom
